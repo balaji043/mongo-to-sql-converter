ccf516220ac84471e2f9e6ff28185185aa443370
Balaji R <cubanhog.royalgamma@gmail.com>
2021-12-27 01:17:36 +0530

    added implementation for limit

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-
-const converter = require('./src/converter')
-
-/**
- *
- * @param {string} input
- * @param {boolean} removeUnderscoreBeforeID
- *
- * @returns {string}
- * @description Convert MongoDB query to SQL query. removeUnderscoreBeforeID flag, if true will convert _id to id, default false
- */
+const converter = require('./src/converter');
+const express = require('express');
+const app = express();
 const convertToSQL = (input, removeUnderscoreBeforeID = false) => {
-  if (typeof input !== 'string') {
-    throw new Error('Input must be type string')
-  }
-  return converter.produceSQL(input, removeUnderscoreBeforeID)
-}
+	if (typeof input !== 'string') {
+		throw new Error('Input must be type string');
+	}
+	return converter.produceSQL(input, removeUnderscoreBeforeID);
+};
+const port = 3000;
+app.get('/:input', (req, res) => {
+	console.log();
+	result = convertToSQL(req.params.input);
+	res.send(result);
+});
 
-module.exports = {
-  convertToSQL
-}
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/converter.js
+++ src/converter.js
@@ -1,23 +1,28 @@
-
-const mongoParse = require('mongo-parse')
+const mongoParse = require('mongo-parse');
 
 const {
-  operatorsMap,
-  parseObjFromString,
-  buildWhereElement
-} = require('./utils')
-const { MONGO_QUERY_OPERATOR } = require('./constants')
+	operatorsMap,
+	parseObjFromString,
+	buildWhereElement,
+} = require('./utils');
+const { MONGO_QUERY_OPERATOR } = require('./constants');
 
 const produceSQL = (input, removeUnderscoreBeforeID) => {
-  const { fromClausePrepared, queryStatements } = parseStructure(input.trim())
-  const { whereClausePrepared, selectClausePrepared } = parseStatements(queryStatements.trim())
+	const { fromClausePrepared, queryStatements } = parseStructure(input.trim());
+	const { whereClausePrepared, selectClausePrepared, limitNumber } = parseStatements(
+		queryStatements.trim()
+	);
 
-  return buildSQL({
-    selectClausePrepared,
-    fromClausePrepared,
-    whereClausePrepared
-  }, removeUnderscoreBeforeID)
-}
+	return buildSQL(
+		{
+			selectClausePrepared,
+			fromClausePrepared,
+			whereClausePrepared,
+			limitNumber,
+		},
+		removeUnderscoreBeforeID
+	);
+};
 
 /**
  *
@@ -27,54 +32,54 @@ const produceSQL = (input, removeUnderscoreBeforeID) => {
  * Returns the collection(table) name and string with query parameters
  */
 const parseStructure = (originalQuery) => {
-  /**
-   * parsing strategy is to use indexOf(char) and then slice() instead if split(char)
-   * the reason: it is possible LIKE statement to contain a dot
-   * */
-  if (originalQuery.length < 1) {
-    throw Error('Empty query')
-  }
-  // check if query starts with db
-  let index = originalQuery.indexOf('.')
-  if (index === -1) {
-    throw Error('Wrong format')
-  }
-  const prefix = originalQuery.slice(0, index)
-  if (prefix !== 'db') {
-    throw Error('Wrong format')
-  }
+	/**
+	 * parsing strategy is to use indexOf(char) and then slice() instead if split(char)
+	 * the reason: it is possible LIKE statement to contain a dot
+	 * */
+	if (originalQuery.length < 1) {
+		throw Error('Empty query');
+	}
+	// check if query starts with db
+	let index = originalQuery.indexOf('.');
+	if (index === -1) {
+		throw Error('Wrong format index -1');
+	}
+	const prefix = originalQuery.slice(0, index);
+	if (prefix !== 'db') {
+		throw Error('Wrong format db');
+	}
 
-  // parse the collection name
-  let rest = originalQuery.slice(index + 1)
-  index = rest.indexOf('.')
-  if (index === -1) {
-    throw Error('Wrong format')
-  }
-  const fromClausePrepared = rest.slice(0, index)
-  if (fromClausePrepared.length < 1) {
-    throw Error('Missing collection name')
-  }
+	// parse the collection name
+	let rest = originalQuery.slice(index + 1);
+	index = rest.indexOf('.');
+	if (index === -1) {
+		throw Error('Wrong format collection name');
+	}
+	const fromClausePrepared = rest.slice(0, index);
+	if (fromClausePrepared.length < 1) {
+		throw Error('Missing collection name');
+	}
 
-  // the only supported method is 'find'
-  rest = rest.slice(index + 1)
-  index = rest.indexOf('(')
-  const method = rest.slice(0, index)
-  if (method !== 'find') {
-    throw Error('Wrong or not supported MongoDB method (' + method + ')')
-  }
+	// the only supported method is 'find'
+	rest = rest.slice(index + 1);
+	index = rest.indexOf('(');
+	const method = rest.slice(0, index);
+	if (method !== 'find') {
+		throw Error('Wrong or not supported MongoDB method (' + method + ')');
+	}
 
-  // cut the semicolon from the end of query if exists
-  let lastChar = rest.length
-  if (rest[lastChar - 1] === ';') {
-    lastChar -= 1
-  }
-  const queryStatements = rest.slice(index, lastChar)
+	// cut the semicolon from the end of query if exists
+	let lastChar = rest.length;
+	if (rest[lastChar - 1] === ';') {
+		lastChar -= 1;
+	}
+	const queryStatements = rest.slice(index, lastChar);
 
-  return {
-    fromClausePrepared,
-    queryStatements
-  }
-}
+	return {
+		fromClausePrepared,
+		queryStatements,
+	};
+};
 
 /**
  *
@@ -83,42 +88,60 @@ const parseStructure = (originalQuery) => {
  * Parse SELECT and WHERE clauses to JS-readable format
  */
 const parseStatements = (originalStatements) => {
-  // check if 'find' followed by parenthized statement
-  if (originalStatements[0] !== '(' || originalStatements[originalStatements.length - 1] !== ')') {
-    throw Error('Method is not parenthized')
-  }
+	// check if 'find' followed by parenthized statement
+	if (
+		originalStatements[0] !== '(' ||
+		originalStatements[originalStatements.length - 1] !== ')'
+	) {
+		throw Error('Method is not parenthized');
+	}
+	let limitNumber = undefined;
+	const isLimitIncluded = originalStatements.includes('limit');
+	if (isLimitIncluded) {
+		const limitIndex = originalStatements.indexOf('limit');
+		limitNumber = originalStatements[limitIndex + 6];
+		originalStatements = originalStatements.replace(
+			`.limit(${limitNumber})`,
+			''
+		);
+		console.log({ originalStatementsAfterLimitRemoved: originalStatements });
+	}
 
-  /**
-   * convert the string containing query(cleaned from brackets) to JS object(array)
-   * using parseObjFromString function from utils.js
-   */
-  const preparedForObjectParsing = '[' + originalStatements.slice(1, originalStatements.length - 1) + ']'
-  const query = parseObjFromString(preparedForObjectParsing)
+	/**
+	 * convert the string containing query(cleaned from brackets) to JS object(array)
+	 * using parseObjFromString function from utils.js
+	 */
+	const preparedForObjectParsing =
+		'[' + originalStatements.slice(1, originalStatements.length - 1) + ']';
 
-  // parse where clause to JS-readable format
-  const whereParsed = mongoParse.parse(query[0])
-  // parse select clause to JS-readable format
-  const selectParsed = mongoParse.parse(query[1])
+	const query = parseObjFromString(preparedForObjectParsing);
+	console.log({ query });
+	// parse where clause to JS-readable format
+	const whereParsed = mongoParse.parse(query[0]);
+	// parse select clause to JS-readable format
+	const selectParsed = mongoParse.parse(query[1]);
 
-  // build usable prepared WHERE
-  const whereClausePrepared = whereParsed.parts.reduce((prev, curr) => [
-    ...prev, prepareWhereClause(curr)
-  ], [])
+	// build usable prepared WHERE
+	const whereClausePrepared = whereParsed.parts.reduce(
+		(prev, curr) => [...prev, prepareWhereClause(curr)],
+		[]
+	);
 
-  // build usable prepared SELECT
-  const selectClausePrepared = selectParsed.parts.reduce((prev, curr) => {
-    if (curr.operand === 1) {
-      return [...prev, curr.field]
-    } else {
-      return null
-    }
-  }, [])
+	// build usable prepared SELECT
+	const selectClausePrepared = selectParsed.parts.reduce((prev, curr) => {
+		if (curr.operand === 1) {
+			return [...prev, curr.field];
+		} else {
+			return null;
+		}
+	}, []);
 
-  return {
-    whereClausePrepared,
-    selectClausePrepared
-  }
-}
+	return {
+		whereClausePrepared,
+		selectClausePrepared,
+		limitNumber,
+	};
+};
 
 /**
  *
@@ -127,30 +150,30 @@ const parseStatements = (originalStatements) => {
  * Build usable prepared WHERE element taking into account nested elements
  */
 const prepareWhereClause = (currentMongoParserElement) => {
-  const { field, operator, operand } = currentMongoParserElement
+	const { field, operator, operand } = currentMongoParserElement;
 
-  // AND or OR operators with nested elements
-  if (typeof field === 'undefined') {
-    // parse nested elements
-    const nested = operand.reduce((prev, curr) => {
-      const parsed = mongoParse.parse(curr)
-      return [...prev, prepareWhereClause(parsed.parts[0])]
-    }, [])
+	// AND or OR operators with nested elements
+	if (typeof field === 'undefined') {
+		// parse nested elements
+		const nested = operand.reduce((prev, curr) => {
+			const parsed = mongoParse.parse(curr);
+			return [...prev, prepareWhereClause(parsed.parts[0])];
+		}, []);
 
-    // nested WHERE element
-    return {
-      field: MONGO_QUERY_OPERATOR,
-      operator: operatorsMap[operator],
-      operand: nested
-    }
-  }
-  // simple WHERE element
-  return {
-    field,
-    operator: operatorsMap[operator],
-    operand
-  }
-}
+		// nested WHERE element
+		return {
+			field: MONGO_QUERY_OPERATOR,
+			operator: operatorsMap[operator],
+			operand: nested,
+		};
+	}
+	// simple WHERE element
+	return {
+		field,
+		operator: operatorsMap[operator],
+		operand,
+	};
+};
 
 /**
  *
@@ -160,30 +183,44 @@ const prepareWhereClause = (currentMongoParserElement) => {
  * Construct final SQL query
  */
 const buildSQL = (describedQuery, removeUnderscoreBeforeID) => {
-  const { selectClausePrepared, fromClausePrepared, whereClausePrepared } = describedQuery
+	const {
+		selectClausePrepared,
+		fromClausePrepared,
+		whereClausePrepared,
+		limitNumber,
+	} = describedQuery;
 
-  if (removeUnderscoreBeforeID) {
-    selectClausePrepared.forEach((element, index) => {
-      if (element === '_id') {
-        selectClausePrepared[index] = 'id'
-      }
-    })
-  }
-  // build WHERE string clause by adding each element of array to it, separated with AND
-  const whereClauseSQL = whereClausePrepared.reduce((prev, curr) => [
-    ...prev, buildWhereElement(curr, removeUnderscoreBeforeID)
-  ], []).join(' AND ')
+	if (removeUnderscoreBeforeID) {
+		selectClausePrepared.forEach((element, index) => {
+			if (element === '_id') {
+				selectClausePrepared[index] = 'id';
+			}
+		});
+	}
+	// build WHERE string clause by adding each element of array to it, separated with AND
+	const whereClauseSQL = whereClausePrepared
+		.reduce(
+			(prev, curr) => [
+				...prev,
+				buildWhereElement(curr, removeUnderscoreBeforeID),
+			],
+			[]
+		)
+		.join(' AND ');
 
-  const select = 'SELECT ' + (selectClausePrepared.length > 0 ? selectClausePrepared.join(', ') : '*')
-  const from = 'FROM ' + fromClausePrepared
-  const where = 'WHERE ' + whereClauseSQL
+	const select =
+		'SELECT ' +
+		(selectClausePrepared.length > 0 ? selectClausePrepared.join(', ') : '*');
+	const from = 'FROM ' + fromClausePrepared;
+	const where = 'WHERE ' + whereClauseSQL;
+	const limit = limitNumber ? 'LIMIT ' + limitNumber : '';
 
-  return select + ' ' + from + ' ' + where + ';'
-}
+	return select + ' ' + from + ' ' + where + ' ' + limit + ';';
+};
 
 module.exports = {
-  produceSQL,
-  parseStructure,
-  parseStatements,
-  buildSQL
-}
+	produceSQL,
+	parseStructure,
+	parseStatements,
+	buildSQL,
+};
